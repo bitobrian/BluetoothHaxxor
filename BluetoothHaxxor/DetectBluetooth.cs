d73da02328ac489bbae84afd664914ad09dfa709
Brian Donaldson <nairbsd@gmail.com>
2023-10-21 18:21:05 -0500

    init

---
+++ BluetoothHaxxor/DetectBluetooth.cs
@@ -0,0 +1,40 @@
+﻿using InTheHand.Net.Sockets;
+
+namespace BluetoothHaxxor;
+
+public class DetectBluetooth
+{
+    public void DetectBluetoothDevices()
+    {
+        BluetoothClient bluetoothClient = new BluetoothClient();
+        var tacos = true;
+
+        while (tacos)
+        {
+            Console.WriteLine("Scanning for devices...");
+
+            var devices = bluetoothClient.DiscoverDevices();
+
+            if (devices.Count > 0)
+            {
+                Console.WriteLine($"Found {devices.Count} devices");
+
+                foreach (var device in devices)
+                {
+                    Console.WriteLine($"Found device: {device.DeviceName} - {device.DeviceAddress}");
+                }
+                
+                tacos = false;
+            }
+            else
+            {
+                Console.WriteLine("No devices found");
+            }
+
+            Thread.Sleep(1000);
+        }
+
+    }
+}
+
+
---
+++ BluetoothHaxxor/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BluetoothHaxxor;
+
+Console.WriteLine("Hello, World!");
+
+var btd = new DetectBluetooth();
+
+btd.DetectBluetoothDevices();
